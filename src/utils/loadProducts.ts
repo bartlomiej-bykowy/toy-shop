29133f52521ae3b63a60c3d7f478c52b10bb3fb7
Bartłomiej Bykowy <bartlomiej.bykowy@gmail.com>
2025-12-04 11:48:59 +0100

    Move data folder to /public

--- src/utils/loadProducts.ts
+++ src/utils/loadProducts.ts
@@ -1,7 +1,7 @@
 import { productsStore, type Product } from "../store";
 
 export async function loadProducts(): Promise<void> {
-  const data = await fetch("/src/data/products.json");
+  const data = await fetch("/data/products.json");
   const products: Product[] = await data.json();
   productsStore.loadProducts(products);
 }
